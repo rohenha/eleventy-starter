029ff84bd8f4a88874e08bf6387138e7e671b500
Romain Breton <romain.breton.pro@gmail.com>
2024-07-31 17:52:55 +0200

    add twig

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,18 @@
-// const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
-
-// const filters = require('./utils/filters.js')
-// const transforms = require('./utils/transforms.js')
-// const shortcodes = require('./utils/shortcodes.js')
-
 import EleventyVitePlugin from '@11ty/eleventy-plugin-vite'
+import EleventyPluginTwig from '@factorial/eleventy-plugin-twig'
 
 import filters from './utils/filters.js'
 import transforms from './utils/transforms.js'
 import shortcodes from './utils/shortcodes.js'
+
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+
+/* ─────────────────────────────────────────────────────── */
+
+const filename = fileURLToPath(import.meta.url)
+const globDirname = dirname(filename)
+const folder = path.resolve(globDirname, 'sources/')
 
 export default function (eleventyConfig) {
 	eleventyConfig.setServerPassthroughCopyBehavior('copy');
@@ -20,12 +24,12 @@ export default function (eleventyConfig) {
 
 		// Vite options (equal to vite.config.js inside project root)
 		viteOptions: {
-			// resolve: {
-			// 	alias: {
-			// 		'@styles/': `${folder}/styles/`,
-			// 		'@scripts/': `${folder}/scripts/`,
-			// 	},
-			// },
+			resolve: {
+				alias: {
+					'@styles/': `${folder}/styles/`,
+					'@scripts/': `${folder}/scripts/`,
+				},
+			},
 			publicDir: 'public',
 			clearScreen: false,
 			server: {
@@ -49,6 +53,57 @@ export default function (eleventyConfig) {
 		}
 	})
 
+	eleventyConfig.addPlugin(EleventyPluginTwig, {
+		twig: {
+      namespaces: {
+        layouts: "layouts",
+        snippets: "snippets"
+      }
+    },
+    mixManifest: "mix-manifest.json",
+    assets: {
+      root: "public",
+      base: "assets",
+      css: "styles",
+      js: "scripts",
+      images: "images",
+    },
+		images: {
+			widths: [300, 600, 900],
+			formats: ["webp", "avif", "jpeg"],
+			additionalAttributes: "",
+		},
+    dir: {
+      src: "views",
+      input: '',
+      layouts: "layouts",
+      output: '_dist',
+    }
+	})
+
+	// eleventyConfig.addPlugin(eleventyPluginTwig, {
+  //   twig: {
+  //     namespaces: {
+  //       layouts: "views/layouts",
+  //       snippets: "views/snippets"
+  //     }
+  //   },
+  //   mixManifest: "mix-manifest.json",
+  //   assets: {
+  //     root: "src",
+  //     base: "assets",
+  //     css: "styles",
+  //     js: "styles",
+  //     images: "images",
+  //   },
+  //   dir: {
+  //     src: "views",
+  //     input: 'views/templates',
+  //     layouts: "views/layouts",
+  //     output: 'www',
+  //   }
+  // })
+
 	// Filters
 	Object.keys(filters).forEach((filterName) => {
 		eleventyConfig.addFilter(filterName, filters[filterName])
@@ -65,7 +120,7 @@ export default function (eleventyConfig) {
 	})
 
 	// Layouts
-	eleventyConfig.addLayoutAlias('base', 'base.njk')
+	eleventyConfig.addLayoutAlias('base', 'base.twig')
 
 	// Copy/pass-through files
 	eleventyConfig.addPassthroughCopy({
@@ -74,8 +129,8 @@ export default function (eleventyConfig) {
 	})
 
 	return {
-		templateFormats: ['njk'],
-		htmlTemplateEngine: 'njk',
+		templateFormats: ['twig'],
+		htmlTemplateEngine: 'twig',
 		passthroughFileCopy: true,
 		dir: {
 			input: 'views',
